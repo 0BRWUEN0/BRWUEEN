44494d043fea00936b7e5c373bca5cb97dc2c3da
0BRWUEN0 <aassas2017@bk.ru>
2017-02-06 23:56:29 +0300

    Update ar-banhammer.lua

--- plugins/ar-banhammer.lua
+++ plugins/ar-banhammer.lua
@@ -140,7 +140,7 @@ local function run(msg, matches)
 local support_id = msg.from.id
  if matches[1]:lower() == 'ايدي' and msg.to.type == "chat" or msg.to.type == "user" then
     if msg.to.type == "user" then
-      return "❣ ايدي البوت : "..msg.to.id.. "\n\n❣ ايدي حسابك : "..msg.from.id.. "\n\n❣ #المطور @SAJJADNOORI"
+      return "❣ ايدي البوت : "..msg.to.id.. "\n\n❣ ايدي حسابك : "..msg.from.id.. "\n\n❣ #المطور @BRWUEN"
     end
     if type(msg.reply_id) ~= "nil" then
       local print_name = user_print_name(msg.from):gsub("‮", "")
